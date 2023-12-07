5df55bd1bbb339a2a8b1e4971b142754f67d8ede
Dardo Altobelli <altobellidardo@gmail.com>
2023-12-06 22:22:38 -0300

    refactor: new middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,34 @@
-export { default } from 'next-auth/middleware'
+import { withAuth } from 'next-auth/middleware'
+import { NextResponse } from 'next/server'
 
+const privatePaths = ['/*']
+const isPrivate = (path) => {
+  return privatePaths.find((x) =>
+    path.match(new RegExp(`^${x}$`.replace('*$', '($|/)')))
+  )
+}
+
+const authMiddleware = withAuth(
+  function onSuccess (req) {
+    console.log('entering a private route')
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => token != null
+    },
+    pages: {
+      signIn: '/auth/login'
+    },
+    secret: process.env.NEXTAUTH_SECRET
+  }
+)
+
+export default function middleware (req) {
+  if (isPrivate(req.nextUrl.pathname)) {
+    return (authMiddleware)(req)
+  }
+  return NextResponse.next()
+}
 export const config = {
   matcher: ['/', '/create', '/update/:path*', '/reviews/:path*', '/user']
 }
