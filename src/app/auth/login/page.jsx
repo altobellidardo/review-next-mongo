721ed0a940728daf9e53ca42e699fce52218d9e0
Dardo Altobelli <altobellidardo@gmail.com>
2023-12-06 22:32:35 -0300

    fix: bad middleware

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -28,12 +28,6 @@ function LoginPage () {
         callbackUrl: '/'
       })
       if (res.ok) {
-        await signIn('credentials', {
-          username: data.username,
-          password: data.password,
-          redirect: false,
-          callbackUrl: '/'
-        })
         setError(null)
         router.push('/')
       } else throw new Error(res.error)
